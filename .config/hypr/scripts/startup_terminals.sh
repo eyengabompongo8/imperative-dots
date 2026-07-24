3bb9393983058973cf6efaddc52b48050e854ffe
Eyenga Bompongo <eyengayolba@gmail.com>
2026-07-24 15:23:19 +0200

    tweaked startup settings

--- .config/hypr/scripts/startup_terminals.sh
+++ .config/hypr/scripts/startup_terminals.sh
@@ -1,15 +1,20 @@
-!#/bin/bash
+#!/bin/bash
 
-# 1. Switch to workspace 8
+sleep 2
+
+# 1. Switch to workspace 0
 hyprctl dispatch workspace 8
 
 # 2. Dynamically set the layout to dwindle
+sleep 0.3
 hyprctl keyword general:layout dwindle
 
 # 3. Launch the 3 terminals with their respective commands on that workspace
+sleep 0.3
 hyprctl dispatch exec "[workspace 8] kitty -e zsh -c 'fastfetch; exec zsh'"
-sleep 0.1
-hyprctl dispatch exec "[workspace 8] kitty -e cava"
-sleep 0.1
 hyprctl dispatch layoutmsg "preselect r"
+sleep 0.3
+hyprctl dispatch exec "[workspace 8] kitty -e cava"
+sleep 0.3
+hyprctl dispatch layoutmsg "preselect d"
 hyprctl dispatch exec "[workspace 8] kitty -e cmatrix"
